9604edd76337bbf6f59ecea3f66d0c45937e8dd8
Maxim Topciu <maximtop@gmail.com>
2016-09-26 21:46:36 +0300

    finally removes all errors

--- src/check_score_clj/core.clj
+++ src/check_score_clj/core.clj
@@ -5,9 +5,15 @@
 (defn get-winner [score]
   (let [[first second] (str/split score #":")]
     (cond
-      (< (read-string first) (read-string second)) :second
-      (> (read-string first) (read-string second)) :first
+      (< (Integer/parseInt first) (Integer/parseInt second)) :second
+      (> (Integer/parseInt first) (Integer/parseInt second)) :first
      :else :draw)))
+
+(defn score [guessed, real]
+  (cond
+    (= guessed real) 2
+    (= (get-winner guessed) (get-winner real)) 1
+    :else 0))
 
 (defn score [guessed, real]
   (cond
--- test/check_score_clj/core_test.clj
+++ test/check_score_clj/core_test.clj
@@ -4,11 +4,17 @@
 
 (deftest check-score-clj-test
   (testing "If player guessed right score result must return 2"
-    (are [expected real guessed] (= expected (score real guessed))
+    (are [expected guessed real] (= expected (score guessed real))
       2 "5:5" "5:5"
       2 "1:2" "1:2"
-      2 "2:1" "2:2")
-  (testing "If player guessed right match result function must return 1"
-    (is (= 1 (score "6:7" "5:6"))))
-  (testing "If player guess was wrong result function must return 0"
-    (is (= 0 (score "6:7" "5:4")))))
+      2 "2:1" "2:1"))
+  (testing "If player guessed right match function must return 1"
+    (are [expected guessed real] (= expected (score guessed real))
+      1 "1:1" "0:0"
+      1 "2:1" "3:1"
+      1 "1:2" "2:3"))
+  (testing "If player guess was wrong function must return 0"
+    (are [expected guessed real] (= expected (score guessed real))
+      0 "1:2" "2:1"
+      0 "1:2" "2:2"
+      0 "2:1" "1:3")))
